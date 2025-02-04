4ee7465ec2f6ddd993e96c2955401edc5b95374c
aishanukovic <aishanukovic@gmail.com>
2025-02-04 01:03:50 -0800

    Created logout routes and logic

--- routes/auth.js
+++ routes/auth.js
@@ -43,7 +43,7 @@ router.get('/logout', (req, res, next) => {
     req.logout(err => {
         if (err) return next(err);
         req.session.destroy();
-        res.redirect('/login');
+        res.redirect('/');
     });
 });
 
